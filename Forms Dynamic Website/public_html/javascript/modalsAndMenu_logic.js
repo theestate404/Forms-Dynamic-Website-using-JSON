36192e503c0007a9d5464129f4d2a66630b93bac
theestate404 <221351473+theestate404@users.noreply.github.com>
2026-03-08 12:33:08 +0000

    Add files via upload

--- Forms Dynamic Website/public_html/javascript/modalsAndMenu_logic.js
+++ Forms Dynamic Website/public_html/javascript/modalsAndMenu_logic.js
@@ -1,55 +1,44 @@
-function openInfoModal(target)
-{
+function openInfoModal(target) {
     displayInfoModal(target);
-    document.getElementById("infoModal").showModal(); 
+    document.getElementById("infoModal").showModal();
 }
-function closeInfoModal()
-{
+function closeInfoModal() {
     document.getElementById("infoModal").close();
 }
-function openDatabaseTable()
-{
+function openDatabaseTable() {
     document.getElementById("tagManager").style.display = "none";
     document.getElementById("databaseTable").style.display = "block";
     document.getElementById("addManager").style.display = "none";
-    document.getElementById("editTargetWindow").style.display="none";
-    document.getElementById("editExampleWindow").style.display="none";
+    document.getElementById("editTargetWindow").style.display = "none";
 }
-function openTagManager() 
-{
+function openTagManager() {
     renderTags();
 
     document.getElementById("tagManager").style.display = "block";
     document.getElementById("databaseTable").style.display = "none";
     document.getElementById("addManager").style.display = "none";
-    document.getElementById("editTargetWindow").style.display="none";
-    document.getElementById("editExampleWindow").style.display="none";
+    document.getElementById("editTargetWindow").style.display = "none";
+    console.log(displayTags);
 }
-function openModifyTagModal()
-{
-    document.getElementById("modifyTagModal").showModal(); 
+function openModifyTagModal() {
+    document.getElementById("modifyTagModal").showModal();
 }
-function closeModifyTagModal()
-{
+function closeModifyTagModal() {
     document.getElementById("modifyTagModal").close();
 }
 
 //OPENING THE ADD TARGET
-function openAddManager()
-{
+function openAddManager() {
     document.getElementById("tagManager").style.display = "none";
     document.getElementById("databaseTable").style.display = "none";
-    document.getElementById("editTargetWindow").style.display="none";
-    document.getElementById("editExampleWindow").style.display="none";
+    document.getElementById("editTargetWindow").style.display = "none";
     document.getElementById("addManager").style.display = "block";
 }
-function toggleMenu() 
-{
+function toggleMenu() {
     const menu = document.getElementById("navMenu");
     menu.classList.toggle("show");
 }
-function deleteConfirmationWindow(targetNum)
-{
+function deleteConfirmationWindow(targetNum) {
     //populating <p> message
     let message = document.getElementById("messageDelete");
     //build message
@@ -58,93 +47,6 @@ function deleteConfirmationWindow(targetNum)
     message.innerHTML = htmlString;
     document.getElementById("deleteModal").showModal();
 }
-function closeDeleteConfirmationWindow()
-{
+function closeDeleteConfirmationWindow() {
     document.getElementById("deleteModal").close();
-}
-function displayEditTarget(id,target,description){
-    document.getElementById("tagManager").style.display = "none";
-    document.getElementById("databaseTable").style.display = "none";
-    document.getElementById("addManager").style.display = "none";
-    document.getElementById("editTargetWindow").style.display="block";
-    
-    let htmlString =`
-    <h2>Modifying Target</h2>
-            <form id="modifyTargetForm">
-                <div>
-                    <div class="card">
-                    <label>Target</label>
-                    <input type="text" id="targetNumModify" value ="${target}">
-                    </div>
-                    <div class="card">
-                    <label>Description</label>
-                    <input type="text" id="descriptionModify" value="${description}">
-                    </div>
-                    <div>
-                        <button type="button" onclick="modifyTarget(${id})">Modify</button>
-                        <button type="button" onclick="openDatabaseTable()">Cancel</button>
-                    </div>
-                </div> 
-            </form>
-    
-`;
-    document.getElementById("editTargetWindow").innerHTML = htmlString;
-}
-function modifyTarget(id){
-    let newTargetNum = document.getElementById("targetNumModify").value;
-    let newDescription = document.getElementById("descriptionModify").value;
-    for(let i=0;i<goal.targets.length;i++){
-        if(goal.targets[i].id===id){
-            goal.targets[i].number = newTargetNum;
-            goal.targets[i].description = newDescription;
-        }
-    }
-    //this updates memory in the table
-    displayData();
-    //this only UI and doesn't updates the table
-    openDatabaseTable();
-}
-function displayEditExample(title, description, images, tags, rating, favourite){
-    closeInfoModal();
-    document.getElementById("tagManager").style.display = "none";
-    document.getElementById("databaseTable").style.display = "none";
-    document.getElementById("addManager").style.display = "none";
-    document.getElementById("editExampleWindow").style.display="block";
-    
-    let htmlString =`
-    <h2>Modifying Example</h2>
-            <form id="modifyExampleForm">
-                <div>
-                   <div class="card">
-                    <label>Title</label>
-                    <input type = "text" value = "${title}">
-                    </div>
-    <div class="card">
-                    <label>Description</label>
-                    <input type = "text" value = "${description}">
-    </div>
-    <div class="card">
-                    <label>Images</label>
-                    <input type = "text"  value = "${images}">
-    </div>
-    <div class="card">
-                    <label>Taggs</label>
-                    <input type = "text" value = "${tags}">
-    </div>
-    <div class="card">
-                    <label>Rating</label>
-                    <input type = "text" value = "${rating}">
-    </div>
-    <div class="card">
-                    <label>Favourite</label>
-                    <input type = "text" value = "${favourite}">
-    </div>
-                    <div>
-                        <button type="button">Modify</button>
-                        <button type="button" onclick="openDatabaseTable()">Cancel</button>
-                    </div>
-                </div>
-            </form>
-`;
-    document.getElementById("editExampleWindow").innerHTML = htmlString;
 }
